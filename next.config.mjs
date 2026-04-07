b34d0bf153eb25e2e5f00ef5391dd1e267086aa4
Ro <xyzchu@hotmail.com>
2026-04-07 16:28:15 +1000

    show time in build date footer

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_BUILD_DATE: new Date().toISOString().slice(0, 10),
+    NEXT_PUBLIC_BUILD_DATE: new Date().toISOString().slice(0, 16).replace('T', ' '),
   },
 }
 
